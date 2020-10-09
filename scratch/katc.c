6d81f3406a698b9e44960acdfcd73a630a17f912
Pete Suchsland <visiondoctor2020@gmail.com>
2020-10-09 11:38:28 +0700

    katc and kats upload

---
+++ scratch/katc.c
@@ -0,0 +1,39 @@
+/*  katc.c
+ *
+ *  This is the fgetc fputc version of kats
+ *
+ *  @author: Peter Suchsland
+ *
+ *
+ *
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+
+int main(int argc, char *argv[]) {
+
+    FILE *file_in;
+    char ch;
+    size_t len = 0;
+    ssize_t read;
+
+    if (argc == 1 ) {   // No args given
+        printf("Please enter a valid filename.\n");
+        return(0);
+    } else {  //  Use the first argument 
+        file_in = fopen(argv[1], "rb"); // Needs an if-file-exists check
+    }
+
+    while(1) {
+        ch = fgetc(file_in);
+        if (ch == EOF) 
+            break;
+        fputc(ch, stdout);
+    }
+
+    fclose(file_in);
+    return(0);
+}
---
+++ scratch/kats.c
@@ -0,0 +1,39 @@
+/*
+ * kats.c
+ * 
+ * This is the fputs fgets version.   
+ * 
+ * My version of cat.  Takes no arguments other than a valid file.
+ * If an invalid file is given... all hell breaks loose.
+ * So... it needs work.
+ * @author: Peter Suchsland
+ *
+ */
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[]) {
+
+    FILE *file_in;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if (argc == 1 ) { 
+       fputs("Please enter a valid filename.\n", stdout); 
+        return(0);
+    } else {  //  Take the first argument to command line
+        file_in = fopen(argv[1], "rb");
+    }
+
+    char string[100];
+
+    while (fgets(string, sizeof(string), file_in) != NULL) 
+        fputs(string, stdout); // write to console
+    
+    fclose(file_in);
+    return(0);
+}
+
